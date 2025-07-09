df6150ba7433954eab8a5c5ff0233f2c8fe791d5
Gerasimos Tsiamalos <tsiger@gmail.com>
2025-07-09 18:47:39 +0300

    Update Vite config
    Removed '**/logs/**' from the ignored watch paths in the Vite server configuration

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     port: 3000,
     watch: {
-      ignored: ["**/data/**", "**/themes/**", "**/logs/**"],
+      ignored: ["**/data/**", "**/themes/**"],
     },
   },
 });
